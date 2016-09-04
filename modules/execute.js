601cbf49b741db0a019710a8e7e6944eb49c0d35
Brian Dennis <briandennis321@gmail.com>
2016-09-04 15:14:55 -0500

    write execute sites command

--- modules/execute.js
+++ modules/execute.js
@@ -1,1 +1,42 @@
-module.exports = () => 2
+const spawn = require('child_process').spawn;
+
+module.exports = (api, collection) => {
+
+  switch (collection) {
+    case 'sites':
+      executeSites(api)
+      break
+
+    case 'commands':
+      executeCommands(api)
+      break
+
+    default:
+      executeSites(api)
+      executeCommands(api)
+  }
+}
+
+function executeSites (api) {
+
+  const openCommands = {
+    darwin: 'open',
+    win32: 'start',
+    linux: 'xdg-open'
+  }
+
+  const sites = api.get('sites')
+
+  if (sites.length) {
+    const command = sites.reduce( (prev, site, index) => {
+      const seperator = index === sites.length ? '' : '|'
+      return `${prev} ${openCommands(process.platform)} ${site} ${seperator}`
+    }, '')
+
+    spawn(command)
+  }
+}
+
+function executeCommands (api) {
+
+}
