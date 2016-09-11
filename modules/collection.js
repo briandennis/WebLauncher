dc2a46b4b569c99d22a265289b81a3167569e1c1
Brian Dennis <briandennis321@gmail.com>
2016-09-10 21:41:59 -0500

    generalize web file to all collections

--- modules/collection.js
+++ modules/collection.js
@@ -3,47 +3,47 @@ const list = require('./list')
 const error = require('./error')
 const execute = require('./execute')
 
-module.exports = (api, args) => {
+module.exports = (api, args, collection) => {
 
   const command = args.splice(0, 1)[0]
 
   switch (command) {
     case 'list':
     case 'ls':
-      list(api, 'sites')
+      list(api, collection)
       break
 
     case 'add':
-      addSites(api, args)
+      addSites(api, args, collection)
       break
 
     case 'clear':
-      clearSites(api, args)
+      clearSites(api, args, collection)
       break
 
     case undefined:
-      execute(api, 'site')
+      execute(api, collection, collection)
       break
   }
 }
 
-function addSites (api, args) {
+function addSites (api, args, collection) {
   if (args.length) {
-    api.add('sites', args)
+    api.add(collection, args)
   } else {
     error(`${chalk.underline.bgRed('add')} must be followed by URLS to be added.`)
   }
 }
 
-function clearSites (api, args) {
+function clearSites (api, args, collection) {
   const validatedArgs = args
     .map( arg => parseInt(arg))
     .filter( arg => !isNaN(arg))
 
   if (args.length && validatedArgs.length === args.length) {
-    api.remove('sites', validatedArgs)
+    api.remove(collection, validatedArgs)
   } else if (!args.length) {
-    api.remove('sites')
+    api.remove(collection)
   } else {
     error(`${chalk.underline.bgRed('clear')} must have 0 or more integer arguments.`)
   }
--- modules/execute.js
+++ modules/execute.js
@@ -1,4 +1,4 @@
-const exec = require('child_process').exec;
+const exec = require('child_process').exec
 
 module.exports = (api, collection) => {
 
