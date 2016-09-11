5d0668a9aeaa5fe0f6ffd400f8a688579fbf6e3e
Brian Dennis <briandennis321@gmail.com>
2016-09-11 16:12:18 -0500

    generalize execute

--- modules/execute.js
+++ modules/execute.js
@@ -2,38 +2,17 @@ const exec = require('child_process').exec
 
 module.exports = (api, collection) => {
 
-  switch (collection) {
-    case 'sites':
-      executeSites(api)
-      break
-
-    case 'commands':
-      executeCommands(api)
-      break
-
-    default:
-      executeSites(api)
-      executeCommands(api)
-  }
-}
-
-function executeSites (api) {
-
   const openCommands = {
     darwin: 'open',
     win32: 'start',
     linux: 'xdg-open'
   }
 
-  const sites = api.get('sites').map( (site) => {
-    return `https://${site}`
+  const sites = api.get(collection).map( (site) => {
+    return `http://${site}`
   })
 
   if (sites.length) {
     exec(`${openCommands[process.platform]} ${sites.join(' ')}`)
   }
-}
-
-function executeCommands (api) {
-
 }
